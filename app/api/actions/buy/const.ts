6d2e797c8fa186b8bf47b16e846c6b829f2aa4dc
Brian Lewis <52287356+solanacryptodev@users.noreply.github.com>
2024-07-25 18:01:56 -0400

    Added asset images to shadow drive and incorporated them into the blink (#2)

--- app/api/actions/buy/const.ts
+++ app/api/actions/buy/const.ts
@@ -1,14 +1,41 @@
+export interface AssetMetadata {
+    name: string;
+    param: string;
+    image: string;
+    mint: string;
+}
+
 export const ATLAS: string = 'ATLASXmbPQxBUYbxPsV97usA3fPQYEqzQBUHgiFCUsXx';
 
-export const assets = [
+export const assets: AssetMetadata[] = [
     {
         name: 'Pearce X4',
         param: 'pearcex4',
+        image: 'https://shdw-drive.genesysgo.net/6CKtcXBouZ4mGAt4NzNcdW29rZCBMx3CTZtBn3zGmz6z/pearceX4.png',
         mint: '2iMhgB4pbdKvwJHVyitpvX5z1NBNypFonUgaSAt9dtDt',
+    },
+    {
+        name: 'Pearce X5',
+        param: 'pearcex5',
+        image: '',
+        mint: ''
+    },
+    {
+        name: 'Pearce R8',
+        param: 'pearcer8',
+        image: '',
+        mint: ''
+    },
+    {
+        name: 'Calico ATS Enforcer',
+        param: 'calicoatsenforcer',
+        image: '',
+        mint: ''
     },
     {
         name: 'Calico Maxhog',
         param: 'calicomaxhog',
+        image: 'https://shdw-drive.genesysgo.net/6CKtcXBouZ4mGAt4NzNcdW29rZCBMx3CTZtBn3zGmz6z/calicomaxhog%20(1).png',
         mint: 'GxpbUDxYYvxiUejHcAMzeV2rzdHf6KZZvT86ACrpFgXa',
     }
 ]
